bb0f499fd57cf973200a7f15fd25af870680c74a
Yudhajit Chowdhury <110385709+berryBatKnave@users.noreply.github.com>
2023-09-18 00:19:36 +0530

    Time: 1 ms (99.85%), Space: 45.1 MB (17.83%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> s = new ArrayList<>();
+        int i=1;
+        while(i<=n){
+            if(i%3==0 && i%5==0){
+                s.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                s.add("Fizz");
+            }
+            else if(i%5==0){
+                s.add("Buzz");
+            }
+            else{
+                s.add(i+"");
+            }
+            i++;
+        }
+        return s;
+        
+    }
+}
