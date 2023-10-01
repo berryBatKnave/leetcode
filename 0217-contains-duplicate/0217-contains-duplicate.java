3d6ac06596e225801925c965fbf53e302de553cc
Yudhajit Chowdhury <110385709+berryBatKnave@users.noreply.github.com>
2023-10-01 12:49:24 +0530

    Time: 10 ms (84.65%), Space: 54.6 MB (92.62%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(hs.contains(nums[i])==false){
+                hs.add(nums[i]);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
